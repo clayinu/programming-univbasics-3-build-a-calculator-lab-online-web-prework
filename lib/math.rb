aef0df7a67458fc7c4422231be92169145ed5f30
Hoko Le <hokole527@gmail.com>
2019-09-23 09:33:52 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  add = num1 + num2
+  return add
 end
 
 def subtraction(num1, num2)
-
+  sub = num1 - num2
+  return sub
 end
 
 def division(num1, num2)
-
+  division = num1 / num2
+  return division
 end
 
 def multiplication(num1, num2)
-
+  multiplication = num1 * num2
+  return multiplication
 end
 
 def modulo(num1, num2)
-
+  modulo = num1 % num2
+  return modulo
 end
 
 def square_root(num)
-
+  return Math.sqrt(num)
 end
